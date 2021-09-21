bc2975df1f2d8c16ad71d91c59463c1231f4b7be
deshraj <sahildeshraj98@gmail.com>
2021-09-22 03:29:20 +0530

    map is added using mapbox

---
+++ index.js
@@ -0,0 +1,17 @@
+function updatemap()
+{
+    fetch("/data.json")
+    .then(response => response.json())
+    .then(rsp => {
+        console.log(rsp.data)
+        rsp.data.forEach(element => {
+            latitude = element.latitude;
+            longitude = element.longitude;
+
+            
+        });
+    })
+
+}
+
+updatemap();
